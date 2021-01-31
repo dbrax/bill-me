43b6c940b640880cf695ffd86308969c026fccc6
dbrax <epmnzava@gmail.com>
2021-01-31 21:51:40 +0300

    modified formatting

--- src/BillMe.php
+++ src/BillMe.php
@@ -1,8 +1,15 @@
 <?php
 
+/**
+ * Author: Emmanuel Paul Mnzava
+ * Twitter: @epmnzava
+ * Github:https://github.com/dbrax/tigopesa-tanzania
+ * Email: epmnzava@gmail.com
+ * 
+ */
+
 namespace Epmnzava\BillMe;
 
 class BillMe
 {
-    // Build your next great package.
 }
