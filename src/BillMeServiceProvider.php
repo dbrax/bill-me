95f48737129251cb61958328fde1d1fd0b5f2dae
dbrax <epmnzava@gmail.com>
2021-02-06 14:21:07 +0300

    made some fixes on namespaces

--- src/BillMeServiceProvider.php
+++ src/BillMeServiceProvider.php
@@ -27,6 +27,7 @@ class BillMeServiceProvider extends ServiceProvider
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
+        
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'bill-me');
 
         if ($this->app->runningInConsole()) {
