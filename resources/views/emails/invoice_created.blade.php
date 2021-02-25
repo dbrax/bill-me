ffc65a1cd38e66df9a38ab17c2bfb7b170f1f806
emmanuel <epmnzava@gmail.com>
2021-02-25 17:34:25 +0300

    added breaks

--- resources/views/emails/invoice_created.blade.php
+++ resources/views/emails/invoice_created.blade.php
@@ -1,12 +1,9 @@
 @component('mail::message')
-@php
-    $i=1;
-@endphp 
+
 Hello {{$invoice->firstname}} , {{$invoice->lastname}}
-Please find your invoice for Your Order
-| SN            | Item                           | Amount                                               |
-| ------------- |:------------------------------:| ----------------------------------------------------:|                       
-| {{$i}}        | {{$invoice->invoice_title}}    | {{config('bill-me.currency')}}{{$invoice->amount}}   |
+Please find your invoice for Your Order \
+{{$invoice->invoice_title}}  \
+{{config('bill-me.currency')}}{{$invoice->amount}} 
 
 
 Thanks,<br>
